7def6c2632a80b74db36fa4dee886a36ef6ebf32
CondorHalcon <judahhohawi@outlook.com>
2024-03-27 16:11:20 -0700

    Namespace Refactor

--- Program.cs
+++ Program.cs
@@ -4,23 +4,29 @@ namespace TradeME
 {
     internal class Program
     {
+        public static GameManager? game;
+
         static void Main(string[] args)
         {
             Raylib.InitWindow(800, 480, "TradeME");
-            Game game = new Game();
+            Raylib.SetTargetFPS(60);
+            game = new GameManager();
+            game.Init();
+            //game.CreateStocks();
 
             while (!Raylib.WindowShouldClose())
             {
                 Raylib.BeginDrawing();
                 Raylib.ClearBackground(Color.WHITE);
 
+                // Update order
                 game.Update();
+                game.ui.Update();
 
-                Raylib.DrawFPS(0, 0);
-                Time.UpdateTime();
+                //Raylib.DrawFPS(0, 0);
                 Raylib.EndDrawing();
+                Time.UpdateTime();
             }
-
             Raylib.CloseWindow();
         }
     }
--- Stock.cs
+++ Stock.cs
@@ -1,15 +1,65 @@
-﻿using System.Collections.Generic;
+﻿//using System.Collections.Generic;
+
+using System.Text.Json.Serialization;
 
 namespace TradeME
 {
-    internal class Stock
+    public class Stock
     {
-        internal string name = "Stock";
-        internal string tag = "STK";
-        internal List<Entry> entries = new List<Entry>();
+        [JsonIgnore(Condition = JsonIgnoreCondition.Never)]
+        public string name = "Stock";
+        [JsonIgnore(Condition = JsonIgnoreCondition.Never)]
+        public string tag = "STK";
+        [JsonIgnore(Condition = JsonIgnoreCondition.Never)]
+        public List<Entry> entries = new List<Entry>();
 
-        internal Entry[] day = new Entry[24];
-        internal Entry[] week = new Entry[7];
-        internal Entry[] month = new Entry[28];
+        [JsonIgnore] public float price
+        {
+            get
+            {
+                if (entries.Count == 0) { return 0; }
+                return entries.Last().price;
+            }
+        }
+
+        // divides the entries to 24 per day and returns the most recent da
+        [JsonIgnore] public Entry[] today
+        {
+            get
+            {
+                List<Entry> list = new List<Entry>();
+                int z = entries.Count - ((entries.Count % 24 == 0) ? 24 : (entries.Count % 24));
+                for (int i = z - 1; i < entries.Count; i++) { list.Add(entries[i]); }
+                return list.ToArray();
+            }
+        }
+        [JsonIgnore] public Entry[] week = new Entry[7];
+        [JsonIgnore] public Entry[] month = new Entry[28];
+
+        #region Constructors
+        public Stock(string name, string tag, float price)
+        {
+            this.name = name;
+            this.tag = tag;
+            this.entries.Add(new Entry(price));
+        }
+        public Stock(string name, string tag, List<Entry> e)
+        {
+            this.name = name;
+            this.tag = tag;
+            this.entries = e;
+        }
+        #endregion
+
+        #region Operators
+        [JsonIgnore] public Entry this[int index] => entries[entries.Count - index - 1];
+
+        public static Stock operator +(Stock lhs, Entry rhs)
+        {
+            lhs.entries.Add(rhs);
+            return lhs;
+        }
+        public static Stock operator +(Entry lhs, Stock rhs) { return rhs + lhs; }
+        #endregion
     }
 }
